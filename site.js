cdbc226edbed0bb7a08338c45ad27877773dafa5
15HayHay <hayley@princesspausch>
2024-11-10 16:22:11 -0500

    Add pictures and stuff

--- site.js
+++ site.js
@@ -5,7 +5,5 @@ const moveCursor = (e) => {
   const mouseY = e.clientY;
   const mouseX = e.clientX;
   cursorPointed.style.transform = `translate3d(${mouseX}px, ${mouseY}px, 0)`;
-  console.log(`mouse(${mouseX}, ${mouseY})`);
-  console.log(cursorPointed.style.transform);
 };
 window.addEventListener("mousemove", moveCursor);
